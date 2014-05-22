c20555b82b2fe170cce0e9a12be372b68ba23cb0
JohnZhang <millinch@gmail.com>
2014-05-22 22:48:54 +0800

    添加Marked的示例

---
+++ marked/assets/main.js
@@ -0,0 +1,9 @@
+﻿require.config({
+	baseUrl: "assets",
+　　paths: {
+		"jquery": "http://cdn.bootcss.com/jquery/2.1.1/jquery",	　　
+	　　"marked": "marked/marked",
+		"highlight": "highlight/highlight.pack",
+		"prettify": "prettify/prettify"
+　　}
+});
