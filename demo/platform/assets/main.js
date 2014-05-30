7221bc9ee21c190de44aef2d502310328b32de66
JohnZhang <johntostring@gmail.com>
2014-05-30 17:39:32 +0800

    平台首页Demo

---
+++ demo/platform/assets/main.js
@@ -0,0 +1,11 @@
+﻿require.config({
+    baseUrl: 'WEB-INF/assets/',
+    paths: {
+		//"jquery": "//cdn.bootcss.com/jquery/2.1.1/jquery",	　　
+		"jquery.min": "jquery/1.11.1/jquery.min",
+	　　"marked": "marked/marked",
+		"highlight": "highlight/highlight.pack",
+		"prettify": "prettify/prettify",
+		"unslider.min": "plugins/unslider.min"
+    }
+});
